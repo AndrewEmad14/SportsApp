d1c8a6b5f200b4dc63ecd45c440a7bd5dee6b82d
Andrew Emad <andrew14emad@gmail.com>
2025-01-30 15:40:54 -0800

    Initial Commit

---
+++ SportsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SportsApp
+//
+//  Created by Andrew Emad on 30/01/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
